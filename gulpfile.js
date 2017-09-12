aae4092da47afd15e692c7f52f84a05f59240cfc
Borodin Dmitriy <demansen@yandex.ru>
2017-09-13 02:36:31 +0300

    automatized tests

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ var gulp = require('gulp'),
   nodemon = require('gulp-nodemon'),
   plumber = require('gulp-plumber'),
   livereload = require('gulp-livereload'),
-  less = require('gulp-less');
+  less = require('gulp-less'),
+  selenium = require('selenium-standalone'),
+  path = require('path'),
+  webdriver = require('gulp-webdriver');
 
 gulp.task('less', function () {
   gulp.src('./public/css/*.less')
@@ -33,6 +36,26 @@ gulp.task('develop', function () {
   });
 });
 
+gulp.task('selenium', function(done){
+  selenium.install({
+    logger: function (message) {}
+  }, function (err) {
+    if (err) 
+      return done(err);
+    selenium.start(function (err, child) {
+      if (err) return done(err);
+      selenium.child = child;
+      done();
+    });
+  });
+});
+
+gulp.task('wdio', ['selenium'], function() {
+  return gulp.src(path.join(__dirname, 'wdio.conf.js'))
+    .pipe(webdriver()).once('end', function() {
+      selenium.child.kill();
+    });
+  });
 gulp.task('default', [
   'less',
   'develop',
