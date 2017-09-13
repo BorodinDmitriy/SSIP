0b8e54837805cf3722e813f253ad6de08041317e
Borodin Dmitriy <demansen@yandex.ru>
2017-09-13 03:12:20 +0300

    form processing

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,11 +1,14 @@
+
 var express = require('express'),
   router = express.Router(),
   mongoose = require('mongoose'),
   Article = mongoose.model('Article');
 
+// jQuery, jsdom
 module.exports = function (app) {
   app.use('/', router);
 };
+
 
 router.get('/', function (req, res, next) {
   Article.find(function (err, articles) {
@@ -16,3 +19,14 @@ router.get('/', function (req, res, next) {
     });
   });
 });
+
+router.post('/', function (req,res) {
+	var login = req.body.login;
+	var password = req.body.password;
+
+	if ((login == 123) && (password == 123)){
+		res.render('success');
+	} else {
+		res.render('failure');
+	}
+})
